62a25cc8c3fea5ea39acc767d2eef622d5729998
BeniBrice <bizimnabenibrice@gmail.com>
2026-02-13 17:33:45 +0200

    fix:site_map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,30 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-
-    {
-      url: "https://www.guangzhouxiang.org/#about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.guangzhouxiang.org/#services",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.guangzhouxiang.org/#portfolio",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.guangzhouxiang.org/#contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ];
 }
