cf4b61fbeac0d6acb10a7641c17951bf83edea35
BeniBrice <bizimnabenibrice@gmail.com>
2026-02-13 17:20:48 +0200

    add:site_map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,37 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.guangzhouxiang.org',
+      url: "https://www.guangzhouxiang.org",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
-  ]
+
+    {
+      url: "https://www.guangzhouxiang.org/#about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.guangzhouxiang.org/#services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.guangzhouxiang.org/#portfolio",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.guangzhouxiang.org/#contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
 }
